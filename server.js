cb5f844a9ee3d4418915307f5a345c463c274c80
Archit Sharma <sarchit1704@gamil.com>
2026-03-08 14:42:10 +0530

    final edit done

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "frontend")));
 
 // app.get("/", (req, res) => {
 //     res.send("server is running");
